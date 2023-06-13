665fbd07c8bf0ac54d510609a9ab0312398f6887
dananjaya2002 <127879066+dananjaya2002@users.noreply.github.com>
2023-06-13 20:05:14 +0530

    added

---
+++ Practical01/Question01.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+//getting output
+    printf("My name is danidu kavishan.\n");
+    printf("My school is Mahinda Rajapaksha collage homagama.\n");
+}
+
---
+++ Practical01/Question07.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+//assigning variables
+    int x,y;
+//getting inputs
+    printf("Enter the first number:- ");
+    scanf("%d",&x);
+    printf("Enter the second number:- ");
+    scanf("%d",&y);
+//getting output
+    printf("Before swap x is %d and y is %d \n",x,y);
+    printf("After swap x is %d and y is %d \n",y,x);
+}
+
