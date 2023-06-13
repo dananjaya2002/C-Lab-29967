aba7823ae271b13527311f6eeb7523f3db683421
dananjaya2002 <127879066+dananjaya2002@users.noreply.github.com>
2023-06-13 20:19:54 +0530

    added

---
+++ Practical04/Question02.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main()
+{
+//assigning variables
+int n1,n2;
+char t;
+//getting input
+printf("Enter the first Number: ");
+scanf("%d", &n1);
+printf("Enter the second Number: ");
+scanf("%d", &n2);
+printf("select the mathematical operation under these operations \n1.Addition type +\n2.Substraction type -\n3.Division type /\n4.Multiplication type *\n ");
+scanf(" %c",&t);
+
+ // switch statement
+    switch (t) {
+        case '+':
+            printf("addition of %d and %d is %d", n1,n2,(n1+n2));
+            break;
+        case '-':
+            printf("subtraction of %d and %d is %d", n1,n2,(n1-n2));
+            break;
+        case '/':
+            printf("division of %d and %d is %d", n1,n2,(n1/n2));
+            break;
+        case '*':
+            printf("multiplication of %d and %d is %d", n1,n2,(n1*n2));
+            break;
+        default:
+            printf("%c is not categorized under +,-,/,* mathematical operations ", t);
+            break;
+
+}
+}
---
+++ Practical04/Question05.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+//assigning variables
+int m;
+
+//getting input
+printf("Enter the month number ");
+scanf("%d",&m);
+
+//switch statement
+switch (m) {
+    case 1:
+    case 3:
+    case 5:
+    case 7:
+    case 8:
+    case 10:
+    case 12:
+        printf("month %d has 31 days",m);
+        break;
+    case 2:
+        printf("month %d has 28 days",m);
+    default:
+        printf("month %d has 30 days",m);
+
+   }
+}
