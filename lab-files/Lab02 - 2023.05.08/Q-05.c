d914a7661c481434af490836bc6e02f1507ed1fe
dananjaya2002 <127879066+dananjaya2002@users.noreply.github.com>
2023-07-26 22:56:24 +0530

    Add files via upload

---
+++ lab-files/Lab02 - 2023.05.08/Q-05.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+//assigning variables
+    float x,y,average;
+//getting input
+    printf("Enter the first number- ");
+    scanf("%f",&x);
+    printf("Enter the second number- ");
+    scanf("%f",&y);
+//process
+    average=(x+y)/2;
+//getting output
+    printf("Average is %f",average);
+
+}
